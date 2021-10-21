02e9d52e3b6451a950933eb0ae0e8c201ae0772b
Bx Hu <45419358+Gustavus6324@users.noreply.github.com>
2021-10-21 19:07:17 +0800

    更新produce方法调用

--- src/main/java/org/example/utils/java_py_test.java
+++ src/main/java/org/example/utils/java_py_test.java
@@ -7,23 +7,17 @@ import java.io.InputStreamReader;
 public class java_py_test {
 
     public static String judge_one(String arg) {
-        // long begin = System.currentTimeMillis();
         Process proc;
         String res = null;
         try {
-            String[] args1 = new String[] { "python3", "/Users/gibbon/Desktop/fabcar/java/src/main/java/org/example/utils/EC-ZKRP/ZKRP_veirfy.py", arg };
+            String[] args1 = new String[] { "/usr/bin/python3",
+                    "/home/hbx/Desktop/Vote/src/main/java/org/example/utils/EC-ZKRP/ZKRP_verify.py", arg };
             proc = Runtime.getRuntime().exec(args1);
             BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
             String line = null;
-            // long t2 = System.currentTimeMillis();
             res = in.readLine();
-            // System.out.println(res);
-            // long end = System.currentTimeMillis();
             in.close();
             proc.waitFor();
-            // System.out.println("总时延为" + (end - begin) + "ms");
-            // System.out.println("调用时间为" + (t2 - t1) + "ms");
-            // System.out.println("读入执行结果时间为" + (end - t2) + "ms");
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -31,5 +25,26 @@ public class java_py_test {
             e.printStackTrace();
         }
         return res;
+    }
+
+    public static String produce(String rep) {
+        Process proc2;
+        String res2 = null;
+        try {
+            String[] args2 = new String[] { "/usr/bin/python3",
+                    "/home/hbx/Desktop/Vote/src/main/java/org/example/utils/EC-ZKRP/ZKRP_produce.py", rep };
+            proc2 = Runtime.getRuntime().exec(args2);
+            BufferedReader in2 = new BufferedReader(new InputStreamReader(proc2.getInputStream()));
+            String line2 = null;
+            res2 = in2.readLine();
+            in2.close();
+            proc2.waitFor();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return res2;
     }
 }
