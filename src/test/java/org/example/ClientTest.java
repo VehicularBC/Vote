52e922cee4fc0b945f8d5fffcf31f4f60d54706e
XIXIXI-5753 <79260066+XIXIXI-5753@users.noreply.github.com>
2021-11-27 12:22:33 +0800

    Add files via upload

--- src/test/java/org/example/ClientTest.java
+++ src/test/java/org/example/ClientTest.java
@@ -7,62 +7,51 @@ package org.example;
 import org.junit.Test;
 
 public class ClientTest {
-	@Test
-	public void testFabCar() throws Exception {
-		/* demo */
-//		EnrollAdmin.main(null);
-//		RegisterUser.main(null);
-//		ClientApp.main(null);
+        @Test
+        public void testFabCar() throws Exception {
+                /* demo */
+//              EnrollAdmin.main(null);
+//              RegisterUser.main(null);
+//              ClientApp.main(null);
 
-//		FabricDemo.main(null);
-
-
-		/* plan A -- TCP */
-//		if (false) {
-//			if (blankCar.main(null) != 0) {
-////				caCar.main(null);
-//			}
-//		} else {
-//			EnrollAdmin.main(null);
-//			RegisterUser.main(null);
-//			caCar.main(null);
-//		}
+//              FabricDemo.main(null);
 
 
-		/* plan B -- UDP */
-//		RSAUtils.genKeyPair();  // 生成公钥和私钥
-//		if (false) {
-//			config.getNowDate("公钥" + RSAUtils.keyMap.get(0));
-//			config.getNowDate("私钥" + RSAUtils.keyMap.get(1));
-//		}
+                /* plan A -- TCP */
+//              if (false) {
+//                      if (blankCar.main(null) != 0) {
+////                            caCar.main(null);
+//                      }
+//              } else {
+//                      EnrollAdmin.main(null);
+//                      RegisterUser.main(null);
+//                      caCar.main(null);
+//              }
 
 
-		config.peerHostIp = "192.168.2.177";
-		config.localUserName = "appUser1";
-		config.newUserName = "appUser1";
-//		System.out.println(config.localUserName);
+                /* plan B -- UDP */
+                RSAUtils.genKeyPair();  // 生成公钥和私钥
+                if (false) {
+                        config.getNowDate("公钥" + RSAUtils.keyMap.get(0));
+                        config.getNowDate("私钥" + RSAUtils.keyMap.get(1));
+                }
 
-		try {
-			int ssss = Integer.parseInt(System.getProperty("t"));
-			/*
-			服务器端：mvn test -D t=1
-			客户端： mvn test -D t=2 */
-			if (ssss == 1) {
-				EnrollAdmin.main(null);
-				RegisterUser.main(null);
-				ClientApp.main(null);
-//				serverPlanB.main(null);
-			} else if (ssss == 2) {
-				clientPlanB.main(null);
-				ClientApp.main(null);
-			} else {
-				EnrollAdmin.main(null);
-				RegisterUser.main(null);
-				ClientApp.main(null);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+                int ssss = Integer.parseInt(System.getProperty("t"));
+//              int ssss = 1;
+                /*
+                服务器端：mvn test -D t=1
+                客户端： mvn test -D t=2 */
+                if (ssss == 1) {
+                        EnrollAdmin.main(null);
+                        RegisterUser.main(null);
+                        serverPlanB.main(null);
+                } else if (ssss == 2) {
+                        clientPlanB.main(null);
+//                      serverPlanB.main(null;
+                        ClientApp.main(null);
+                } else if (ssss == 3) {
+                    ClientApp.main(null);
+                }
 
-	}
+        }
 }
